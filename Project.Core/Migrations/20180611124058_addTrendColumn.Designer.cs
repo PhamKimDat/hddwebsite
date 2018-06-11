465a174a0de9491607fbfa57d7c2eb8f3e2bb7b0
PhamKimDat <kimdat171296@gmail.com>
2018-06-11 20:48:34 +0700

    ShowNewProduct

--- Project.Core/Migrations/20180611124058_addTrendColumn.Designer.cs
+++ Project.Core/Migrations/20180611124058_addTrendColumn.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Project.SQLServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContextSQL))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180611124058_addTrendColumn")]
+    partial class addTrendColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
