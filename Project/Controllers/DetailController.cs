7d67a288bebe8786de96d8787040e8da71602445
PhamKimDat <kimdat171296@gmail.com>
2018-06-14 15:56:05 +0700

    Add View For Detail

--- Project/Controllers/DetailController.cs
+++ Project/Controllers/DetailController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Project.Common.Entities;
+using Project.SQLServer.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,11 +12,28 @@ namespace Project.Web.Controllers
 {
     public class DetailController : Controller
     {
+        #region fields
+        private readonly ApplicationDbContextSQL _context;
+        //MySQL
+        //private readonly ApplicationDbContext _context;
+
+        #endregion
+
+        #region Constructor
+        public DetailController(
+            ApplicationDbContextSQL context
+            //ApplicationDbContext context (MySQL)
+            )
+        {
+            _context = context;
+        }
+        #endregion
         // GET: /<controller>/
         public IActionResult Index(string ID)
         {
             ViewData["ID"] = ID;
-            return View();
+            var product = (Product)_context.Products.SingleOrDefault(x => x.Id == ID);
+            return View(product);
         }
     }
 }
