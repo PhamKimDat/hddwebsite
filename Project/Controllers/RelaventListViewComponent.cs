1b50a01fd589399c1618b3abe6ebf16d2b9c5036
Hoàng Duy <hoangduy@Duys-MacBook-Pro.local>
2018-06-15 10:38:42 +0700

    Update detail and home page

---
+++ Project/Controllers/RelaventListViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Project.MySQL.Data;
+using Project.Common.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Web.Controllers
+{
+    public class RelaventListViewComponent : ViewComponent
+    {
+        //MySQL
+        private readonly ApplicationDbContext db;
+
+        public RelaventListViewComponent(ApplicationDbContext context)
+        {
+            this.db = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+            Product p)
+        {
+            var products = await GetItemsAsync(p);
+            return View(products);
+        }
+        private Task<List<Product>> GetItemsAsync(Product p)
+        {
+            return db.Products.Where(x => x.ProductType == p.ProductType).ToListAsync();
+        }
+    }
+}
